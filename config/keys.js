d5e738841d04c6b9d7d567db9f7d65b09cf3802e
aaron <xakong@outlook.com>
2019-02-03 22:35:10 -0500

    prepare to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-   mongoURI: 'mongodb://xaaron3:xaaron3@ds125113.mlab.com:25113/devconnector',
-   secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+   module.exports = require('./keys_prod')
+} else {
+   module.exports = require('./keys_dev')
 }
